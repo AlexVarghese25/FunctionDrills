1c1a45b577d822c80ef8e194c1a7da140699268c
Alex Varghese <alex.varghese250@gmail.com>
2019-02-19 13:32:34 -0500

    seventh commit

--- AgeName.js
+++ AgeName.js
@@ -1,13 +1,21 @@
 'use strict';
 
 function createGreeting(name, age){
-  
-  return `Hi, my name is ${name} and I'm ${age} years old.`;
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. ${yob}'`;
 }
 function getYearOfBirth(age){
+  if(age<0){
+    throw new Error('Age can not be negative');
+  }
   const yearOfBirth = 2016-age;
   return `I was born in ${yearOfBirth}.`;
 }
 
-const greeting = createGreeting('Alex', 25);
-console.log(greeting);
+try{
+  const greeting = createGreeting('Alex', 25);
+  console.log(greeting);
+}
+catch(e){
+  return e;
+}
