45c8b9733e53e370cc8b6772a9524490ab87f7c8
Alex Varghese <alex.varghese250@gmail.com>
2019-02-19 13:10:56 -0500

    second  commit

--- AgeName.js
+++ AgeName.js
@@ -1,1 +1,7 @@
-console.log('Hi, my name is Alex and I\'m 25 years old')
+'use strict';
+
+function createGreeting(){
+    console.log('Hi, my name is Alex and I\'m 25 years old');
+}
+
+createGreeting();
